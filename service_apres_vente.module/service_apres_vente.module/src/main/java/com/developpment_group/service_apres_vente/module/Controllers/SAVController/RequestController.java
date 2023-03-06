8c211394c73361193c6f254be50490084beefba5
Ahmed Ghorbel <ahmed.ghorbel1@esprit.tn>
2023-03-06 22:20:20 +0100

    Project Running-- SAV: Entities, Repo, Service, Controller, Common'Api Response Messaging'

--- service_apres_vente.module/service_apres_vente.module/src/main/java/com/developpment_group/service_apres_vente/module/Controllers/SAVController/RequestController.java
+++ service_apres_vente.module/service_apres_vente.module/src/main/java/com/developpment_group/service_apres_vente/module/Controllers/SAVController/RequestController.java
@@ -1,7 +1,8 @@
 package com.developpment_group.service_apres_vente.module.Controllers.SAVController;
 
 
-import com.developpment_group.service_apres_vente.module.Config.ApiResponse;
+import com.developpment_group.service_apres_vente.module.Common.ApiResponse;
+import com.developpment_group.service_apres_vente.module.Modules.SAV.Request;
 import com.developpment_group.service_apres_vente.module.Services.SAVService.RequestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -23,5 +24,12 @@ public class RequestController {
     private RequestService Request_Service;
 
 
-    
+    @PostMapping("/Create")
+    public ResponseEntity<ApiResponse> createCategory(@Validated @RequestBody Request request) {
+        if (Objects.nonNull(Request_Service.readRequest(request.getOrder()))) {
+            return new ResponseEntity<ApiResponse>(new ApiResponse(false, "Request already exists"), HttpStatus.CONFLICT);
+        }
+        Request_Service.createRequest(request);
+        return new ResponseEntity<>(new ApiResponse(true, "Request has been successfully Added"), HttpStatus.CREATED);
+    }
 }
